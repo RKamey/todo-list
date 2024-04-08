e510a6923715d338b90c340cdafb8b64779150f3
RKamey <ralonso@ucol.mx>
2024-04-07 21:49:42 -0600

    ✨ Added unique identifier for tasks

--- js/Task.js
+++ js/Task.js
@@ -2,9 +2,10 @@ class Task {
     constructor(name, complete = false) {
         this.name = name;
         this.complete = complete;
+        this.id = Date.now();
     }
 
     completeTask() {
-        this.complete = true;
+        this.complete = !this.complete;
     }
 }
