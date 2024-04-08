4684b10e22965b9cbe727661c73fcf5abd97d5e0
RKamey <ralonso@ucol.mx>
2024-04-07 21:53:14 -0600

    ♻️ Updated logic of functions and task handling

--- js/app.js
+++ js/app.js
@@ -5,33 +5,14 @@ const addBtn = document.querySelector('#add-btn');
 const deleteBtn = document.querySelector('#delete-btn');
 const tasksRender = document.querySelector('#tasks-render');
 
+
 // Add task
-addBtn.addEventListener('click', () => {
+function addTask() {
     let task = new Task(taskInput.value);
     taskList.addTask(task);
     renderTask();
     taskInput.value = '';
-});
-
-// Delete task
-tasksRender.addEventListener('click', (e) => {
-    if (e.target.classList.contains('delete-btn')) {
-        let taskName = e.target.parentNode.querySelector('p').textContent;
-        let task = taskList.getTasks().find(t => t.name === taskName);
-        taskList.removeTask(task);
-        renderTask();
-    }
-});
-
-// Complete task
-tasksRender.addEventListener('click', (e) => {
-    if (e.target.classList.contains('complete-btn')) {
-        let taskName = e.target.parentNode.querySelector('p').textContent;
-        let task = taskList.getTasks().find(t => t.name === taskName);
-        task.completeTask();
-        renderTask();
-    }
-});
+}
 
 // Render tasks
 function renderTask() {
@@ -40,3 +21,47 @@ function renderTask() {
         tasksRender.innerHTML += TaskRow(t);
     });
 }
+
+// Add task by clicking the add button
+addBtn.addEventListener('click', addTask);
+
+// Add task by pressing enter
+taskInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        addTask();
+    }
+});
+
+// Find the task by id, e is the event
+function findTask() {
+    let taskId = parseInt(this.getAttribute('data-taskId')); // Get the task id from the button
+    return taskList.getTasks().find(t => t.id === taskId); // Find the task by id
+}
+
+// Delete task
+function deleteTask() {
+    let task = findTask.call(this);
+    taskList.removeTask(task);
+}
+
+// Mark task as complete
+function markAsComplete() {
+    let task = findTask.call(this);
+    task.completeTask(); // Change the task status
+}
+
+// Delete task by clicking the delete button
+tasksRender.addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete-btn')) {
+        deleteTask.call(e.target); // Call the deleteTask function and pass the button as this
+        renderTask();
+    }
+});
+
+// Complete task by clicking the complete button
+tasksRender.addEventListener('click', (e) => {
+    if (e.target.classList.contains('complete-btn')) {
+        markAsComplete.call(e.target); // Call the markAsComplete function and pass the button as this
+        renderTask();
+    }
+});
